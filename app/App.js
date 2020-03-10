c3a48d5a3f403472e53121abacfab63aaf75957a
yoyota <smarthorse6@gmail.com>
2020-03-10 21:17:19 +0900

    Add forward head posture

--- app/App.js
+++ app/App.js
@@ -1,15 +1,51 @@
-import React, { useState, useCallback, useRef } from "react"
+import React, { useState, useCallback } from "react"
+import { Container } from "react-bootstrap"
 import { ipcRenderer } from "electron"
 import ForwardHeadPosture from "react-forward-head-posture"
+import Store from "electron-store"
+
+const store = new Store()
+const q = []
+function onEstimate(under, score) {
+  if (q.length > 9) {
+    q.shift()
+  }
+  q.push(score)
+  const averageScore = q.reduce((prev, curr) => prev + curr, 0) / q.length
+  ipcRenderer.send("score", averageScore)
+  if (averageScore < under) {
+    ipcRenderer.send("alert", averageScore)
+  }
+}
 
 export default function() {
-  const onEstimate = useCallback(score => {
-    // if (q.current.length > 9) {
-    //   q.current.shift()
-    // }
-    // q.current.push(score)
-    // q.current.reduce((prev, curr) => prev + curr, 0) / q.current.length
-    ipcRenderer.send("score", score)
-  }, [])
-  return <ForwardHeadPosture onEstimate={onEstimate} frameRate={5} />
+  const [under, setUnder] = useState(store.get("under", 0))
+  const [score, setScore] = useState(0)
+  const onEstimateCallback = useCallback(
+    scoresArray => {
+      onEstimate(under, scoresArray[0][0])
+      setScore(scoresArray[0][0])
+    },
+    [under]
+  )
+
+  return (
+    <Container>
+      <div className="d-flex">
+        <h4>Alert under : </h4>
+        <input
+          type="number"
+          value={under}
+          onChange={e => {
+            setUnder(e.target.value)
+            store.set("under", e.target.value)
+          }}
+        />
+      </div>
+      <h5>score: {score}</h5>
+      <div style={{ height: "80%", width: "100%" }}>
+        <ForwardHeadPosture onEstimate={onEstimateCallback} frameRate={5} />
+      </div>
+    </Container>
+  )
 }
--- app/createMenubar.js
+++ app/createMenubar.js
@@ -1,18 +1,25 @@
 const { menubar } = require("menubar")
-const { ipcMain, nativeImage, Tray } = require("electron")
+const { ipcMain, nativeImage, Tray, Notification } = require("electron")
 
 const { NODE_ENV } = process.env
 
 let tray
+let notification
 
 ipcMain.on("score", async (_, score) => {
   tray.setTitle(Math.round(score).toString())
+})
+
+ipcMain.on("alert", async (_, score) => {
+  notification.body = `score: ${score}`
+  notification.show()
 })
 
 module.exports = function createMenubar() {
   const pencilDataURL =
     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAAAhFBMVEUAAADs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PHs8PFed9IBAAAAK3RSTlMAAQMEBQYLIScqLC0vMjU2ODk+QkN+f4KGi46SlZeanbe5wMHDyNni8/f9aI/hMAAAAH5JREFUGBmdwUkCgjAQBMCORkTcjUFxBaOi0///n3OKgSNVGGbzytHnSCnQZUqqAin3sCXJLxI7qYL15BV/W9k3DNbfDaK1uIbk0xpES3E11c0gWoirqS4G0VxcTXU2iGafd6A6ISHTrCVZIcV8nLU8ooNcjcIBXVQePROFgX5ezA2dwHGGSQAAAABJRU5ErkJggg=="
   tray = new Tray(nativeImage.createFromDataURL(pencilDataURL))
+  notification = new Notification({ title: "forward head posture" })
   const mb = menubar({
     browserWindow: {
       show: false,
